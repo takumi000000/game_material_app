a3f15cd8cc395922f7bc6d788fbe70d59cdc7249
Takahshi-Keita <79685658+Takahshi-Keita@users.noreply.github.com>
2021-10-07 11:58:20 +0900

    add login function

---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  devise_for :users
+  root 'top#index'
+  resources :contractor
+  resources :requester
+
+  get 'pages/show'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
