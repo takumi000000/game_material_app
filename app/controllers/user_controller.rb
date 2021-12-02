6aac70221cbb3f7f60ae6edc37269fe54ea74f79
ryryryhayashi <77333054+ryryryhayashi@users.noreply.github.com>
2021-12-02 11:56:31 +0900

    マイページ編集ページ作成

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -5,6 +5,16 @@ class UserController < ApplicationController
     @user = User.find(params[:id])
   end
   def edit
-    
+    @user = User.find(params[:id])
+  end
+  def update
+    @user = User.find(params[:id])
+    @user.name = params[:user][:name]
+    @user.introduction = params[:user][:introduction]
+    if @user.save
+      redirect_to user_path
+    else
+      render edit_user_path
+    end
   end
 end
